03916b7ddf1b08ce8f084147f46779af1dfb0352
AsukaruShouhei <asukaru2014@icloud.com>
2019-11-01 11:46:49 +0900

    修正

--- app.py
+++ app.py
@@ -1,12 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template
+import pandas as pd
+import iris_learn
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
+
+
+@app.route('/ayame')
+def ayame_readcsv():
+    df = pd.read_csv('csv/iris.csv')
+    q = df.values.tolist()
+    return render_template('ayamedata.html', data=q)
+
+
+@app.route('/accuracy')
+def accuracy():
+    ress = iris_learn.getIris()
+    return render_template('accuracy.html', ress=ress)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, use_reloader=True, use_debugger=True)
---
+++ iris_learn.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+import urllib.request as req
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+import pandas as pd
+
+
+def getIris():
+    csv = pd.read_csv('csv/iris.csv')
+    y = csv.loc[:, 'Name']
+    x = csv.loc[:, ['SepalLength', 'SepalWidth', 'PetalLength',  'PetalWidth']]
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, train_size=0.8, shuffle=True)
+    clf = SVC()
+    clf.fit(x_train, y_train)
+    y_pred = clf.predict(x_test)
+    return accuracy_score(y_test, y_pred)
